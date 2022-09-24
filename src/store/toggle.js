25f5d86497ebb7c676ef1258fe4d9edc32574cd0
Luan <luanmessias03@gmail.com>
2022-09-24 14:45:46 -0300

    turn dark mode off by default

--- src/store/toggle.js
+++ src/store/toggle.js
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia'
 
 export const useToggleStore = defineStore('toggle', {
   state: () => ({
-    isThemeDarkActive: true,
+    isThemeDarkActive: false,
     isGoalFormActive: false,
     isEditGoalFormActive: false,
     isTaskFormActive: false,
