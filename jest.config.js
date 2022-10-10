180e2f451d7d9a8d5648de9fab75c5135e8163d9
Luan <luanmessias03@gmail.com>
2022-10-10 17:50:29 -0300

    config unit tests coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
   testMatch: ['**/src/**/*.spec.[jt]s?(x)'],
+  moduleFileExtensions: ['js', 'json', 'vue'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{js,vue}', '!src/main.js'],
 }
