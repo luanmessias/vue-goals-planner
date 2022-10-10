28766176ffb5d99dcd86b1fe323253924e026858
Luan <luanmessias03@gmail.com>
2022-10-10 17:03:56 -0300

    working on unit tests

--- src/components/MainHeader/index.spec.js
+++ src/components/MainHeader/index.spec.js
@@ -2,23 +2,22 @@ import { mount } from '@vue/test-utils'
 import Component from './'
 import i18n from '@/plugins/i18n'
 import en from '@/locales/en.json'
+import { createTestingPinia } from '@pinia/testing'
 
 describe('Main Header Component', () => {
   let wrapper
+  const pinia = createTestingPinia()
 
   beforeEach(() => {
+    i18n.global.setLocaleMessage('en', en)
     wrapper = mount(Component, {
       global: {
-        plugins: [i18n],
+        plugins: [pinia, i18n],
       },
     })
   })
 
-  describe('title', () => {
-    it('should be rendered correctly', () => {
-      const title = en['home.main.title']
-      const el = wrapper.find('h1')
-      expect(el.text()).toEqual(title)
-    })
+  it('should render the component', () => {
+    expect(wrapper.exists()).toBe(true)
   })
 })
