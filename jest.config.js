5a8d79a4ee859f5c2fac16f3c6db14e12947b988
Luan <luanmessias03@gmail.com>
2022-10-28 14:50:40 -0300

    Config jest to ignore images and icons on report

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,15 @@ module.exports = {
   moduleFileExtensions: ['js', 'json', 'vue'],
   collectCoverage: false,
   collectCoverageFrom: ['src/**/*.{js,vue}', '!src/main.js'],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/img/',
+    '/imgs/',
+    '/assets/',
+    '/locales/',
+    '/icons/',
+    '/icon/',
+  ],
   transformIgnorePatterns: ['node_modules/(?!vue-material-design-icons)'],
   moduleNameMapper: {
     '^@icons/(.*)$': '<rootDir>/node_modules/vue-material-design-icons/$1',
